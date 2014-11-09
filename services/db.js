8ce3e6135bff5338c339f64f1c03064e45787793
Arun Nair <ariesaru2002@gmail.com>
2014-11-09 00:28:02 -0500

    Finally got correct openshift url for mongo

--- services/db.js
+++ services/db.js
@@ -3,7 +3,7 @@ var MongoClient = require('mongodb').MongoClient;
 var MongoServer = require('mongodb').Server;
 var mongoConnectionClient = new MongoClient(new MongoServer('localhost', 27018, {native_parser: true}));;
 if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-	var url = process.env.OPENSHIFT_MONGODB_DB_URL;	
+	var url = process.env.OPENSHIFT_MONGODB_DB_URL + 'babycare';	
 }
  else {
  	var url = 'mongodb://localhost:27018/BabyCare';
