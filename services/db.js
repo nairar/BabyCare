05ca97eff73f94b98d8c23125abdf3e2649f7cc0
Arun Nair <ariesaru2002@gmail.com>
2014-11-08 23:28:56 -0500

    Adding a new URL with DB instance babycare

--- services/db.js
+++ services/db.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var MongoClient = require('mongodb').MongoClient;
 var MongoServer = require('mongodb').Server;
 var mongoConnectionClient = new MongoClient(new MongoServer('localhost', 27018, {native_parser: true}));;
-var url = process.env['OPENSHIFT_MONGODB_DB_URL'] || 'mongodb://localhost:27018/BabyCare';
+var url = process.env['OPENSHIFT_MONGODB_DB_URL']+'babycare' || 'mongodb://localhost:27018/BabyCare';
 
 var userCollection = mongoConnectionClient.db('userDB');
 
