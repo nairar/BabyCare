13383ac94022db02f3366f32e4432e0a474040c3
Arun Nair <ariesaru2002@gmail.com>
2014-11-09 01:02:33 -0500

    Added logout basic

--- public/auth/auth.js
+++ public/auth/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1499227687028781' || '1492832364316920', // your App ID
         'clientSecret'  : '0398b27db616682cdc5144508c69df19' || '2889758e8107b106f603d154487a0faa', // your App Secret
-        'callbackURL'   : 'http://babycare-nairar.rhcloud.com/auth/facebook/callback'
+        'callbackURL'   : 'http://babycare-nairar.rhcloud.com/auth/facebook/callback' 
     }
 
 }
