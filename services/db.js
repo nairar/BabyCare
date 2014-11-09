bc289469dd8aaf9350bfbb7ce558aca94ea8debe
Arun Nair <ariesaru2002@gmail.com>
2014-11-08 23:53:48 -0500

    Some issues with mongo URL, changed to if-else condition

--- services/db.js
+++ services/db.js
@@ -2,7 +2,12 @@ var mongoose = require("mongoose");
 var MongoClient = require('mongodb').MongoClient;
 var MongoServer = require('mongodb').Server;
 var mongoConnectionClient = new MongoClient(new MongoServer('localhost', 27018, {native_parser: true}));;
-var url = process.env['OPENSHIFT_MONGODB_DB_URL']+'babycare' || 'mongodb://localhost:27018/BabyCare';
+if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+	var url = process.env['OPENSHIFT_MONGODB_DB_URL']+'/babycare';	
+}
+ else {
+ 	var url = 'mongodb://localhost:27018/BabyCare';
+ }
 
 var userCollection = mongoConnectionClient.db('userDB');
 
