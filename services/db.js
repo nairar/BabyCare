2d8d4a64c81367e6c346424e05b0147f2165ddbc
Arun Nair <ariesaru2002@gmail.com>
2014-11-08 23:26:52 -0500

    Adding mongodb openshift url

--- services/db.js
+++ services/db.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 var MongoClient = require('mongodb').MongoClient;
 var MongoServer = require('mongodb').Server;
 var mongoConnectionClient = new MongoClient(new MongoServer('localhost', 27018, {native_parser: true}));;
-var url = 'mongodb://localhost:27018/BabyCare';
+var url = process.env['OPENSHIFT_MONGODB_DB_URL'] || 'mongodb://localhost:27018/BabyCare';
+
 var userCollection = mongoConnectionClient.db('userDB');
 
 exports.userCollection = userCollection;
